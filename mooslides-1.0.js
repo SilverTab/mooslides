2f7ff56cf83dba7379f69f53e48f05d9aa6e0a5f
Jean-Nicolas Jolivet <silvertab@videotron.ca>
2008-06-28 02:57:40 -0400

    Options changed, some more doc

--- mooslides-1.0.js
+++ mooslides-1.0.js
@@ -1,3 +1,8 @@
+/*
+	mooslides-1.0.js by Jean-Nicolas Jolivet (http://www.silverscripting.com)
+	Licenced under the MIT license: http://www.opensource.org/licenses/mit-license.php
+*/
+
 /**
 	Construct a new mooslides object. Make sure that your outterdiv already exists
 	and has been inserted in the DOM. (i.e. create a new mooslide in the window.domready
@@ -5,10 +10,46 @@
     @class Represents a mooslides object
 	@param {String} outterdiv Your main div
 	@param {Object} options A set of options (described below)
-	@param {options} customToolbar
-	@param {options}
-	@example
-	var myslides = new mooslides('mydiv', { options });
+	
+	<dt class="heading">Options</dt>
+	@param {bool} [options.customToolbar='false'] Set to true if you plan on using your 
+	own toolbar. If you do, you will have to come up with your own Next, Previous, ...,
+	buttons. You can use the slideTo(), slideNext(), slidePrevious(), slideFirst() and
+	slideLast() functions to help you out.
+	
+	@param {Fx.Transitions} [options.transitionEffect='Fx.Transitions.Expo.easeOut'] An 
+	Fx.Transitions object that will be used when switching panels.
+	
+	@params {Int} [options.animationDuration='1000'] The duration of the panel switching
+	effect.
+	
+	@param {Int} [options.animationDelay='4000'] Number of milliseconds to wait between 
+	panel switching in animation mode.
+	
+	@param {bool} [options.autoStart='false'] Set to true if you want the animation to 
+	start when the page is loaded.
+	
+	@param {String} [options.buttonClass='toolbar-button'] If you want to use the default toolbar, 
+	you can specify a custom class that will be applied to the toolbar's buttons. This is
+	only so you can control the styles of the toolbar. Checkout the example abouve, it
+	contains a basic style that should get you started.
+	
+	@example 
+	// Creating a new mooslides with those styles.
+	var myslides = new mooslides('mydiv', {
+		customToolbar: false,
+		toolbarStyles: myStyles
+	});
+	
+	// an example CSS style that makes a pretty toolbar
+	li.toolbar-button {
+		display: inline;
+		background-color: #262626;
+		padding: 2px 5px;
+		color: #ddd;
+		cursor: pointer;
+	}
+	
  */ 
 
 var mooslides = new Class({
@@ -16,21 +57,26 @@ var mooslides = new Class({
 	
 	Implements: Options,
 	options: {
-		customToolbar: false
-		
+		customToolbar: false,
+		transitionEffect: Fx.Transitions.Expo.easeOut,
+		animationDelay: 4000,
+		animationDuration: 1000,
+		autoStart: false,
+		buttonClass: 'toolbar-button'
 	},
 	/**
 	@private
 	@constructor
 	*/
 	initialize: function(outterdiv, options) {
+		
 		this.setOptions(options);
 		this.outterdiv = $(outterdiv);
 		this.innerdiv = new Element('div', {
 			id: 'innerdiv'
 		});
 		
-		
+		$extend(this.toolbarStyles, this.options.toolbarStyles);
 		this.panels = this.outterdiv.getChildren().filter(".panels");
 		if(this.options.customToolbar == false) {
 			this.toolbar = this.buildToolbar();
@@ -39,6 +85,7 @@ var mooslides = new Class({
 		else {
 			this.toolbar = this.options.customToolbar;
 		}
+		
 		this.panels.setStyles({
 			float: 'left'
 		});
@@ -52,18 +99,20 @@ var mooslides = new Class({
 		
 		this.outterdiv.setStyle('overflow', 'hidden');
 		this.activePanelId = 0;
+		
 		this.slideTo(0);
+
+		if(this.options.autoStart) {
+			this.loopStart();
+		}
+		
 	},
 	/**
 	Builds the toolbar if the user wants to use the default toolbar. *INTERNAL USE*.
 	@private
 	*/
 	buildToolbar: function() {
-		var newToolbarDiv = new Element('div', {
-			'styles': {
-				'height': '24px'	
-			}
-		});
+		var newToolbarDiv = new Element('div', {});
 		
 		var newToolbarUl = new Element('ul', {
 			'styles': {
@@ -72,19 +121,13 @@ var mooslides = new Class({
 			}
 		});
 		this.previousButton = new Element('li', {
-			'styles': {
-					'display': 'inline',
-					'padding': '2px 10px',
-					'background-color': '#262626',
-					'color': '#eee',
-					'cursor': 'pointer'
-				},
-				'html' : '<<',
-				'events': {
-					'click': function() {
-						this.slidePrevious();
-					}.bind(this)
-				}
+			'class': this.options.buttonClass,
+			'html' : '<<',
+			'events': {
+				'click': function() {
+					this.slidePrevious();
+				}.bind(this)
+			}
 		});
 		this.previousButton.inject(newToolbarUl);
 		var cnt = 1;
@@ -93,13 +136,7 @@ var mooslides = new Class({
 			
 			this.buttons[cnt-1] = new Element('li', {
 				'html': cnt + "",
-				'styles': {
-					'display': 'inline',
-					'padding': '2px 10px',
-					'background-color': '#262626',
-					'color': '#eee',
-					'cursor': 'pointer'
-				},
+				'class': this.options.buttonClass,
 				'title': cnt + ""
 			});
 			this.buttons[cnt-1].addEvent('click', function(aButton) {
@@ -110,13 +147,7 @@ var mooslides = new Class({
 		
 		}.bind(this));
 		this.nextButton = new Element('li', {
-			'styles': {
-					'display': 'inline',
-					'padding': '2px 10px',
-					'background-color': '#262626',
-					'color': '#eee',
-					'cursor': 'pointer'
-				},
+			'class': this.options.buttonClass,
 				'html' : '>>',
 				'events': {
 					'click': function() {
@@ -140,8 +171,8 @@ var mooslides = new Class({
 		panelId = (panelId < 0) ? 0 : panelId;
 		panelId = (panelId > this.panels.length - 1) ? this.panels.length - 1 : panelId;
 		var toX = (panelId ) * this.size.x;
-		var myFx = new Fx.Scroll(this.outterdiv, {duration: 1000, transition: Fx.Transitions.Expo.easeOut}).start(toX, 0);
-		this.activePanelId = panelId;
+		var myFx = new Fx.Scroll(this.outterdiv, {duration: this.options.animationDuration, transition: this.options.transitionEffect}).start(toX, 0);
+		this.changed(panelId);
 	},
 	
 	/**
@@ -190,7 +221,7 @@ var mooslides = new Class({
 	myslides.loopStart();
 	*/
 	loopStart:function () {
-		this.timer = this.loopNext.periodical(4000, this);
+		this.timer = this.loopNext.periodical(this.options.animationDelay, this);
 	},
 	
 	/**
@@ -208,13 +239,14 @@ var mooslides = new Class({
 	*/
 	loopNext: function() {
 		if(this.activePanelId == this.panels.length -1) {
-			var myFx = new Fx.Scroll(this.outterdiv, {duration: 200, transition: Fx.Transitions.Expo.easeOut}).start(0, 0);
-			this.activePanelId = 0;
+			var myFx = new Fx.Scroll(this.outterdiv, {duration: 200, transition: this.options.transitionEffect}).start(0, 0);
+			this.changed(0);
 		}
 		else {
 			this.slideNext();
 		}
 	},
+	
 	
 	/**
 	Similar to slideTo but does it without an animation. This means you will move
@@ -226,6 +258,24 @@ var mooslides = new Class({
 		panelId = (panelId > this.panels.length - 1) ? this.panels.length - 1 : panelId;
 		var toX = (panelId ) * this.size.x;
 		var myFx = new Fx.Scroll(this.outterdiv).set(toX, 0);
-		this.activePanelId = panelId;
+		this.changed(panelId);
+	},
+	
+	/**
+	Changed event. This is mostly for internal use. Note that it fires a "changed" event
+	on the main div. You can catch this event. The id of the newly selected panel
+	will be passed as an argument to your callback function.
+	@event
+	@returns {Int} The id of the newly selected panel.
+	*/
+	changed: function(newPanel) {
+		if(this.options.customToolbar == false)
+		{
+			
+			this.buttons[this.activePanelId].removeClass('active');
+			this.buttons[newPanel].addClass('active');
+		}
+		this.activePanelId = newPanel;
+		this.outterdiv.fireEvent('changed', newPanel);
 	}
 });
